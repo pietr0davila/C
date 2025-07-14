74737ab03a170b1c59e493e0aef6cd777a905215
pietr0davila <dsescuderop@gmail.com>
2025-07-14 11:37:18 -0300

    Tipos de ponteiro

--- pointer_type.c
+++ pointer_type.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    char char_array[5] = {'a', 'a', 'a', 'a', 'a'};
+    char char_array[5] = {'a', 'b', 'c', 'd', 'e'};
     int int_array[5] = {1, 2, 3, 4, 5};
 
     char *char_pointer;
@@ -10,15 +10,12 @@ int main(void) {
     char_pointer = char_array;
     int_pointer = int_array;
 
-    for (int i = 0; i < 5; i++)
-    {
+    for (int i = 0; i < 5; i++) {
         printf("[integer pointer] points to %p, that contains %d\n", int_pointer, *int_pointer);
         int_pointer++;
     }
-    for (int i = 0; i < 5; i++)
-    {
+    for (int i = 0; i < 5; i++) {
         printf("[char pointer] points to %p, that contains %c\n", char_pointer, *char_pointer);
         char_pointer++;
-
     }
 }
